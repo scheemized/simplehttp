5759311f150834567c1b8c887939adeb86290f20
scheemized <junsung.tak00@gmail.com>
2025-07-19 03:56:41 -0700

    adding dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+#include <fcntl.h>
 #include <iostream>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+    std::cout << "Hello World!" << std::endl;
+    printf("%s: Hi, this is from printf\n", __FUNCTION__);
+
+    string f = "hisdf";
+
+    return 0;
 }
